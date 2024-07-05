5270d30d800bad02dfef7253f2a8eca47d467d5a
Susmitha-Pasupuleti <150095411+Susmitha-Pasupuleti@users.noreply.github.com>
2024-07-05 18:56:11 -0500

    Update Me.jsx

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -25,7 +25,7 @@ const Me = () => {
                     variants={container(0)}
                     initial="hidden"
                     animate="visible"
-                     className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Susmitha Pasupuleti </motion.h1>
+                     className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Susmitha Pasupuleti </motion.h1>
                     <motion.span variants={container(0.5)}
                     initial="hidden"
                     animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</motion.span>
