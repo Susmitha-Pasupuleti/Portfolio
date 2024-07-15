a829431dbcdcec28782a3fa7087e18b3896b880d
Susmitha-Pasupuleti <150095411+Susmitha-Pasupuleti@users.noreply.github.com>
2024-07-15 18:17:24 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
 import {ABOUT_TEXT} from '../constants'
+import {motion} from "framer-motion"
 
 const About = () => {
   return (
-    <div className=" border-b border-neutral-900 pb-4">
-        <h1 className="my-10 text-center text-4xl">About
+    <div id="About" className=" border-b border-neutral-900 pb-4 ">
+        <motion.h1 whileInView={{opacity:1,y:0}}
+        initial={{y:-100,opacity:0}}
+        transition={{duration:1.5}}className="my-10 text-center text-4xl">About
             <span className=" text-neutral-500" > Me</span>
-        </h1>
+        </motion.h1>
         <div className="w-full lg:w">
             <div className="flex justify-center lg:justify-start ">
             <p className='my-2 maxw-xl py-0 text-justify'>{ABOUT_TEXT}</p>
