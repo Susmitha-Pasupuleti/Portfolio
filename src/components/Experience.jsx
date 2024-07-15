134d234064004e998e46ab8cebba7907226c7a5a
Susmitha-Pasupuleti <150095411+Susmitha-Pasupuleti@users.noreply.github.com>
2024-07-15 18:30:29 -0500

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import {EXPERIENCES} from '../constants'
 import {motion} from "framer-motion"
 const Experience = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
+    <div id= "Experience" className="border-b border-neutral-900 pb-4">
         <motion.h1 
         whileInView={{opacity:1,y:0}}
         initial={{y:-100,opacity:0}}
